5f5729912fe3df2c95d0f87b6d4f7e98ef4802df
CSE007Arif <35569588+CSE007Arif@users.noreply.github.com>
2018-01-18 10:16:22 -0600

    first commit

---
+++ bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+    Transform player;
+
+    public float speed = 10f;
+    private int damage = 1;
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time. deltaTime);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerInfo player = other.GetComponent<PlayerInfo>();
+        if(player!=null)
+        {
+            player.Hurt(damage);
+        
+        }
+        Destroy(this.gameObject);
+    }
+}
